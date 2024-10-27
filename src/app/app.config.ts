8d531c7bd1ef30c004c7d51466b50d082ff3ff72
Salvador Nicolas <snicoper@gmail.com>
2024-10-27 20:51:34 +0100

    Anadido @angular/material-luxon-adapter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideLuxonDateAdapter(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ],
 };
