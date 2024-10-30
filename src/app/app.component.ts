5df5c49d5320e097270cee5d10354f7b140bf0d3
Salvador Nicolas <snicoper@gmail.com>
2024-10-30 05:28:23 +0100

    Implements dark and light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
+import { ThemeManagerService } from './shared/services/theme-manager.service';
 
 @Component({
   selector: 'am-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  private themeManagerService = inject(ThemeManagerService);
+
+  handleChange() {
+    this.themeManagerService.toggle();
+  }
+}
--- src/app/shared/core/config/app-config.ts
+++ src/app/shared/core/config/app-config.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ThemeManagerService } from '../../services/theme-manager.service';
 
 /** Configuración inicial de la aplicación. */
 @Injectable()
 export class AppConfig {
+  private readonly themeManagerService = inject(ThemeManagerService);
+
   load(): Promise<void> {
     return new Promise((resolve) => {
+      this.themeManagerService.initialize();
       resolve();
     });
   }
