76ae0628079d365283523320212576c8e64aad56
Salvador Nicolas <snicoper@gmail.com>
2024-12-08 11:26:00 +0100

    Fix sort tables

--- src/app/features/accounts/pages/account-create/account-create.component.ts
+++ src/app/features/accounts/pages/account-create/account-create.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PageBaseComponent } from '../../../../shared/components/layout/page-base/page-base.component';
 
 @Component({
   selector: 'am-account-create',
-  imports: [],
+  imports: [PageBaseComponent],
   templateUrl: './account-create.component.html',
   styleUrl: './account-create.component.scss',
 })
-export class AccountCreateComponent { }
+export class AccountCreateComponent {}
