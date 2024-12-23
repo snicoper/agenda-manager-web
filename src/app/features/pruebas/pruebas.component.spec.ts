4c49c970aef74f20cad7bc87f829adc6564784c7
Salvador Nicolas <snicoper@gmail.com>
2024-12-23 17:42:02 +0100

    Fix

---
+++ src/app/features/pruebas/pruebas.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PruebasComponent } from './pruebas.component';
+
+describe('PruebasComponent', () => {
+  let component: PruebasComponent;
+  let fixture: ComponentFixture<PruebasComponent>;
+  let loggerSpy: { logInfo: jasmine.Spy };
+
+  beforeEach(async () => {
+    loggerSpy = jasmine.createSpyObj('logger', ['logInfo']);
+    (window as any).logInfo = loggerSpy.logInfo;
+
+    await TestBed.configureTestingModule({
+      imports: [PruebasComponent],
+      providers: [],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PruebasComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call logInfo in constructor', () => {
+    expect(loggerSpy.logInfo).toHaveBeenCalled();
+  });
+});
