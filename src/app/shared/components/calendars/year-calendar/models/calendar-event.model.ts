6e92679b6a51e106ee0bb073d6784801fce16831
Salvador Nicolas <snicoper@gmail.com>
2024-12-31 10:32:52 +0100

    clean code

--- src/app/shared/components/calendars/year-calendar/models/calendar-event.model.ts
+++ src/app/shared/components/calendars/year-calendar/models/calendar-event.model.ts
@@ -1,11 +1,5 @@
 import { Period } from '../../../../../core/models/period.model';
 
-export interface CalendarEvent {
-  period: Period;
-  name: string;
-  description: string;
-}
-
 export interface CalendarItem {
   period: Period;
   cssClass?: string;
