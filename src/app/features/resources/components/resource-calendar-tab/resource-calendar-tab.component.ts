0038940aa162e1f43e6b8066f55c65e68977a329
Salvador Nicolas <snicoper@gmail.com>
2025-01-15 07:10:28 +0100

    Creados componentes base ResourceSchedulesTabComponent y ResourceCalendarTabComponent

---
+++ src/app/features/resources/components/resource-calendar-tab/resource-calendar-tab.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'am-resource-calendar-tab',
+  imports: [],
+  templateUrl: './resource-calendar-tab.component.html',
+  styleUrl: './resource-calendar-tab.component.scss',
+})
+export class ResourceCalendarTabComponent { }
---
+++ src/app/features/resources/components/resource-schedules-tab/resource-schedules-tab.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'am-resource-schedules-tab',
+  imports: [],
+  templateUrl: './resource-schedules-tab.component.html',
+  styleUrl: './resource-schedules-tab.component.scss',
+})
+export class ResourceSchedulesTabComponent { }
