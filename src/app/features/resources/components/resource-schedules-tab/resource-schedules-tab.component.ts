ec9df64607f9c720dd324e09d01009eda7184356
Salvador Nicolas <snicoper@gmail.com>
2025-01-20 00:25:24 +0100

    Muestra tabla en el HTML

--- src/app/features/resources/components/resource-schedules-tab/resource-schedules-tab.component.ts
+++ src/app/features/resources/components/resource-schedules-tab/resource-schedules-tab.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, inject, signal, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSort, MatSortModule, Sort, SortDirection } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { finalize, take } from 'rxjs';
 import { SystemPermissions } from '../../../../core/modules/auth/constants/system-permissions.const';
 import { PaginatedResult } from '../../../../core/modules/paginated-result/paginated-result';
 import { BladeService } from '../../../../shared/components/blade/services/blade.service';
+import { PaginatorComponent } from '../../../../shared/components/paginator/paginator.component';
+import { TableFilterComponent } from '../../../../shared/components/tables/table-filter/table-filter.component';
 import { RequiredPermissionDirective } from '../../../../shared/directives/required-permission.directive';
+import { BoolToIconPipe } from '../../../../shared/pipes/bool-to-icon.pipe';
+import { DateTimeFormatPipe } from '../../../../shared/pipes/date-time-format.pipe';
 import { ResourceScheduleResponse } from '../../models/responses/resource-schedule.response';
 import { ResourceApiService } from '../../services/api/resource-api.service';
 import { ResourceSelectedStateService } from '../../services/state/resource-selected-state.service';
@@ -16,7 +23,21 @@ import { ScheduleCreateBladeComponent } from '../schedule-create-blade/schedule-
 
 @Component({
   selector: 'am-resource-schedules-tab',
-  imports: [MatButtonModule, MatIconModule, RequiredPermissionDirective],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatIconModule,
+    TableFilterComponent,
+    PaginatorComponent,
+    BoolToIconPipe,
+    DateTimeFormatPipe,
+    RequiredPermissionDirective,
+  ],
   templateUrl: './resource-schedules-tab.component.html',
   styleUrl: './resource-schedules-tab.component.scss',
 })
@@ -80,6 +101,16 @@ export class ResourceSchedulesTabComponent implements AfterViewInit {
         take(1),
         finalize(() => this.isLoading.set(false)),
       )
-      .subscribe();
+      .subscribe({
+        next: (response) => {
+          this.paginatedResult = PaginatedResult.create<ResourceScheduleResponse>(response);
+          this.dataSource.data = this.paginatedResult.items;
+
+          if (this.sort && this.paginatedResult.order) {
+            this.sort.active = this.paginatedResult.order.propertyName;
+            this.sort.direction = this.paginatedResult.order.orderType.toLowerCase() as SortDirection;
+          }
+        },
+      });
   }
 }
